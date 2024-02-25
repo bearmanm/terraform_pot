ce45b67aedeafb68860e4bbb7399f8859d84513d
Bearman, Mike <mike.bearman@sky.uk>
2024-02-25 12:25:13 +0000

    backup of VIP

--- terraform_resources/nov23_month_2/assignment/backend.tf
+++ terraform_resources/nov23_month_2/assignment/backend.tf
@@ -1,8 +1,8 @@
-# BACKEND
+# backend refers to the storage and retrieval mechanism for Terraform state files. 
+# it contains provider and terraform is used to define and provision infrastructure using Hashicorp Terraform
 
 provider "aws" {
-  region                   = "eu-west-2"
-  # shared_credentials_files = ["~/.aws/credentials"] # cred are passing via environment variables # not needed 
+  region = "eu-west-2"
 }
 
 terraform {
@@ -10,13 +10,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "> 5.0"
     }
   }
   backend "s3" {
     region = "eu-west-2"
-    profile = "uk-grpctb-concourse-role" # created a new role in prod using an existing user 'skyuk-deploy-concourse-user' 
-    bucket = "prod-grpctb-state-269632189206-s3" # this is the prod account where the state file will get stored 
-    key = "sky/grpctb/prod-videoipath-state" # sky/grpctb is the path and prod-videoipath-state will be the will name
   }
+}
+
+locals {
+  environment = "prod"
 }
